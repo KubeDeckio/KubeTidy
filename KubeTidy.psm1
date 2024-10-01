b218ba8f7e9435ac0a498be43e378e82416aa59d
Richard Hooper <littlehoops@gmail.com>
2024-10-01 10:21:12 +0100

    Update KubeTidy.psm1

--- KubeTidy.psm1
+++ KubeTidy.psm1
@@ -1,5 +1,5 @@
 # Dot Source all functions in all ps1 files located in this module
-Get-ChildItem -Path $PSScriptRoot\public\*.ps1 | ForEach-Object { . $_.FullName }
+Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 | ForEach-Object { . $_.FullName }
 
 # Dot Source all functions in all ps1 files located in this module
 Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 | ForEach-Object { . $_.FullName }
